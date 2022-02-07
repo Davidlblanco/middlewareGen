0a3128e16b25d5ae0109588fe3ce09cd2d94211c
David Blanco <david.blanco@maeztra.com>
2022-02-07 13:45:11 -0300

    credentials

--- getSheet.js
+++ getSheet.js
@@ -1,12 +1,11 @@
 const { google } = require('googleapis')
-const prod = 'credentialsHeroku.js'
-const dev = 'credential.json'
-
+const GoogleAuth = require('google-auth-library');
 exports.sheet = async () => {
-    const auth = new google.auth.GoogleAuth({
-        keyFile: prod,
-        scopes: 'https://www.googleapis.com/auth/spreadsheets',
-    })
+    // const auth = new google.auth.GoogleAuth({
+    //     keyFile: prod,
+    //     scopes: 'https://www.googleapis.com/auth/spreadsheets',
+    // })
+    const auth = authorize()
     const client = await auth.getClient()
 
     const googleSheets = google.sheets({ version: 'v4', auth: client })
@@ -20,3 +19,17 @@ exports.sheet = async () => {
     return metaData
 }
 
+
+function authorize() {
+    return new Promise(resolve => {
+        const authFactory = new GoogleAuth();
+        const jwtClient = new authFactory.JWT(
+            process.env.client_email, // defined in Heroku
+            null,
+            process.env.private_key, // defined in Heroku
+            ['https://www.googleapis.com/auth/calendar']
+        );
+
+        jwtClient.authorize(() => resolve(jwtClient));
+    });
+}
